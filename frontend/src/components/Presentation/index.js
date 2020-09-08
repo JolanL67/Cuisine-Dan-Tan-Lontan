0a3b71d844480cdb0b4d776bf5f92c0dbae549bd
Tony <gta.ide@hotmail.com>
2020-09-08 11:35:32 +0200

    responsive 80% ok

--- frontend/src/components/Presentation/index.js
+++ frontend/src/components/Presentation/index.js
@@ -3,26 +3,25 @@ import React from 'react';
 import './presentation.scss';
 
 const Presentation = () => (
-  <div className="cdtl_presentation">
-    <div className="cdtl_presentation_header">
-      <h1 className="cdtl_presentation_title">Il etait une fois ...</h1>
+  <>
+    <h1 className="cdtl_presentation_Maintitle">Notre Histoire</h1>
+    <div className="cdtl_presentation">
+      <div className="cdtl_presentation_header">
+        <h1 className="cdtl_presentation_title">Il etait une fois ...</h1>
+      </div>
+      <div className="cdtl_presentation_description">
+        <p>
+          Yannick et rose nous sommes originaire de la réunion,
+          Yannick de saint Paul ( savannah) et moi de saint Clotilde
+          Nous sommes tout les deux de très bon amis et c’est ainsi que
+          Nous avions voulue mettre en place se concept de la cuisine dan tan lontan pour faire partager notre passion et notre plaisir de cuisiner.
+          C’est avec la motivation et les encouragement de nos amis, collègue et famille de notre entourage qui nous a mis cette lancer… avec tout les bons petits plats creole qu’on leur faisaient gouter … auprès d’un repas entre nous.
+          La naissance de projet Ca était réfléchir depuis l’année dernier .  qu’on n’y pensait tout les deux, vue qu’on aime bien cuisiner et de ce qu’on n’a appris auprès de nos parents , la famille a regarde cuisine dans la traditions et en suite c’était une passion pour nous d »être a cote d’eux et apprendre 
+          Nous proposons dns notre concept .  les caris, rougails, achard, pains américains, samoussas, bouchons, bonbons piment, nos gâteaux creole ; en proposant la restauration à emporter (barquettes comme à la Réunion )
+        </p>
+      </div>
     </div>
-    <div className="cdtl_presentation_description">
-      <p>
-        Yannick et rose nous sommes originaire de la réunion,
-        Yannick de saint Paul ( savannah) et moi de saint Clotilde
-Nous sommes tout les deux de très bon amis et c’est ainsi que 
-Nous avions voulue mettre en place se concept de la cuisine dan tan lontan pour faire partager notre passion et notre plaisir de cuisiner.
-
-C’est avec la motivation et les encouragement de nos amis, collègue et famille de notre entourage qui nous a mis cette lancer… avec tout les bons petits plats creole qu’on leur faisaient gouter … auprès d’un repas entre nous.
-
-La naissance de projet Ca était réfléchir depuis l’année dernier .  qu’on n’y pensait tout les deux, vue qu’on aime bien cuisiner et de ce qu’on n’a appris auprès de nos parents , la famille a regarde cuisine dans la traditions et en suite c’était une passion pour nous d »être a cote d’eux et apprendre 
-
-Nous proposons dns notre concept .  les caris, rougails, achard, pains américains, samoussas, bouchons, bonbons piment, nos gâteaux creole ; en proposant la restauration à emporter (barquettes comme à la Réunion )
-      </p>
-      
-    </div>
-  </div>
+  </>
 
 );
 
