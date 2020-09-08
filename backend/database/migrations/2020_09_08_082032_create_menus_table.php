117b55d8282b21f8af539d84012e21bde2407da8
JolanL67 <jolan.lazzari@gmail.com>
2020-09-08 11:21:59 +0200

    Creation database and entities

---
+++ backend/database/migrations/2020_09_08_082032_create_menus_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu', function (Blueprint $table) {
+            $table->id();
+            $table->string('type', 255);
+            $table->string('name', 255);
+            $table->integer('price');
+            $table->string('ingredient', 255);
+            $table->boolean('is_discount');
+            $table->integer('nb_unit');
+            $table->float('discount_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
